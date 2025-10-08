45a73fd990ada07e4d25311f7c74ecffdc35bbd9
Woeter <skippedlead@gmail.com>
2025-10-08 10:01:40 +0530

    Added cstats.cpp

---
+++ cstats.cpp
@@ -0,0 +1,50 @@
+using namespace std;
+
+
+void sortArray(double arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                double temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+double mean(const double data[], int n) {
+    if (n <= 0) return 0.0;
+    double sum = 0.0;
+    for (int i = 0; i < n; i++) {
+        sum += data[i];
+    }
+    return sum / n;
+}
+
+double median(double data[], int n) {
+    if (n <= 0) return 0.0;
+    sortArray(data, n);
+
+    if (n % 2 == 0)
+        return (data[n/2 - 1] + data[n/2]) / 2.0;
+    else
+        return data[n/2];
+}
+
+double range(const double data[], int n) {
+    if (n <= 0) return 0.0;
+    double minVal = data[0];
+    double maxVal = data[0];
+    for (int i = 1; i < n; i++) {
+        if (data[i] < minVal) minVal = data[i];
+        if (data[i] > maxVal) maxVal = data[i];
+    }
+    return maxVal - minVal;
+}
+
+
+
+int main() {
+	
+}
