66f19aa72c7df0b24143389417eb8a4fcda84500
Woeter <skippedlead@gmail.com>
2025-10-13 12:28:24 +0530

    Added more functions

--- cstats/cstats.cpp
+++ cstats/cstats.cpp
@@ -1,4 +1,5 @@
 #include <cmath>
+#include <cstdio>
 #include "cstats.h"
 
 
@@ -109,4 +110,210 @@ double variance(const double data[], int n) {
 
 double stdev(const double data[], int n) {
     return sqrt(variance(data, n));
+}
+
+double percentile(double data[], int n, double p) {
+    if (n <= 0 || p < 0 || p > 100) return 0.0;
+    sortArray(data, n);
+    
+    double index = (p / 100.0) * (n - 1);
+    int lower = (int)index;
+    int upper = lower + 1;
+    
+    if (upper >= n) return data[n - 1];
+    
+    double weight = index - lower;
+    return data[lower] * (1 - weight) + data[upper] * weight;
+}
+
+double quartile1(double data[], int n) {
+    return percentile(data, n, 25.0);
+}
+
+double quartile3(double data[], int n) {
+    return percentile(data, n, 75.0);
+}
+
+double iqr(double data[], int n) {
+    if (n <= 0) return 0.0;
+    double* temp = new double[n];
+    for (int i = 0; i < n; i++) temp[i] = data[i];
+    
+    double q1 = quartile1(temp, n);
+    for (int i = 0; i < n; i++) temp[i] = data[i];
+    double q3 = quartile3(temp, n);
+    
+    delete[] temp;
+    return q3 - q1;
+}
+
+double sampleVariance(const double data[], int n) {
+    if (n <= 1) return 0.0;
+    
+    double m = mean(data, n);
+    double var = 0.0;
+    
+    for (int i = 0; i < n; i++) {
+        var += (data[i] - m) * (data[i] - m);
+    }
+    
+    return var / (n - 1);
+}
+
+double sampleStdev(const double data[], int n) {
+    return sqrt(sampleVariance(data, n));
+}
+
+double meanAbsoluteDeviation(const double data[], int n) {
+    if (n <= 0) return 0.0;
+    
+    double m = mean(data, n);
+    double mad = 0.0;
+    
+    for (int i = 0; i < n; i++) {
+        mad += fabs(data[i] - m);
+    }
+    
+    return mad / n;
+}
+
+double coefficientOfVariation(const double data[], int n) {
+    if (n <= 0) return 0.0;
+    
+    double m = mean(data, n);
+    if (m == 0.0) return 0.0;
+    
+    return (stdev(data, n) / m) * 100.0;
+}
+
+double skewness(const double data[], int n) {
+    if (n <= 2) return 0.0;
+    
+    double m = mean(data, n);
+    double sd = stdev(data, n);
+    if (sd == 0.0) return 0.0;
+    
+    double skew = 0.0;
+    for (int i = 0; i < n; i++) {
+        double z = (data[i] - m) / sd;
+        skew += z * z * z;
+    }
+    
+    return skew / n;
+}
+
+double kurtosis(const double data[], int n) {
+    if (n <= 3) return 0.0;
+    
+    double m = mean(data, n);
+    double sd = stdev(data, n);
+    if (sd == 0.0) return 0.0;
+    
+    double kurt = 0.0;
+    for (int i = 0; i < n; i++) {
+        double z = (data[i] - m) / sd;
+        kurt += z * z * z * z;
+    }
+    
+    return (kurt / n) - 3.0;
+}
+
+double zScore(double value, double mean, double stdev) {
+    if (stdev == 0.0) return 0.0;
+    return (value - mean) / stdev;
+}
+
+double covariance(const double x[], const double y[], int n) {
+    if (n <= 0) return 0.0;
+    
+    double meanX = mean(x, n);
+    double meanY = mean(y, n);
+    
+    double cov = 0.0;
+    for (int i = 0; i < n; i++) {
+        cov += (x[i] - meanX) * (y[i] - meanY);
+    }
+    
+    return cov / n;
+}
+
+double correlation(const double x[], const double y[], int n) {
+    if (n <= 0) return 0.0;
+    
+    double cov = covariance(x, y, n);
+    double sdX = stdev(x, n);
+    double sdY = stdev(y, n);
+    
+    if (sdX == 0.0 || sdY == 0.0) return 0.0;
+    
+    return cov / (sdX * sdY);
+}
+
+double geometricMean(const double data[], int n) {
+    if (n <= 0) return 0.0;
+    
+    double product = 1.0;
+    for (int i = 0; i < n; i++) {
+        if (data[i] <= 0) return 0.0;
+        product *= data[i];
+    }
+    
+    return pow(product, 1.0 / n);
+}
+
+double harmonicMean(const double data[], int n) {
+    if (n <= 0) return 0.0;
+    
+    double sumReciprocals = 0.0;
+    for (int i = 0; i < n; i++) {
+        if (data[i] == 0.0) return 0.0;
+        sumReciprocals += 1.0 / data[i];
+    }
+    
+    return n / sumReciprocals;
+}
+
+double rootMeanSquare(const double data[], int n) {
+    if (n <= 0) return 0.0;
+    
+    double sumSquares = 0.0;
+    for (int i = 0; i < n; i++) {
+        sumSquares += data[i] * data[i];
+    }
+    
+    return sqrt(sumSquares / n);
+}
+
+void copyArray(const double src[], double dest[], int n) {
+    for (int i = 0; i < n; i++) {
+        dest[i] = src[i];
+    }
+}
+
+void printSummaryStats(const double data[], int n) {
+    if (n <= 0) {
+        printf("No data available\n");
+        return;
+    }
+    
+    double* temp = new double[n];
+    copyArray(data, temp, n);
+    
+    printf("=== Summary Statistics ===\n");
+    printf("Count:    %d\n", n);
+    printf("Mean:     %.4f\n", mean(data, n));
+    printf("Median:   %.4f\n", median(temp, n));
+    printf("Std Dev:  %.4f\n", stdev(data, n));
+    printf("Min:      %.4f\n", min(data, n));
+    printf("Max:      %.4f\n", max(data, n));
+    printf("Range:    %.4f\n", range(data, n));
+    
+    copyArray(data, temp, n);
+    printf("Q1:       %.4f\n", quartile1(temp, n));
+    copyArray(data, temp, n);
+    printf("Q3:       %.4f\n", quartile3(temp, n));
+    copyArray(data, temp, n);
+    printf("IQR:      %.4f\n", iqr(temp, n));
+    
+    delete[] temp;
 }
--- cstats/cstats.h
+++ cstats/cstats.h
@@ -16,4 +16,26 @@ double sum(const double data[], int n);
 int count(const double data[], int n);
 double range(const double data[], int n);
 
+// Advanced stats
+double percentile(double data[], int n, double p);
+double quartile1(double data[], int n);
+double quartile3(double data[], int n);
+double iqr(double data[], int n);
+double sampleVariance(const double data[], int n);
+double sampleStdev(const double data[], int n);
+double meanAbsoluteDeviation(const double data[], int n);
+double coefficientOfVariation(const double data[], int n);
+double skewness(const double data[], int n);
+double kurtosis(const double data[], int n);
+double zScore(double value, double mean, double stdev);
+double covariance(const double x[], const double y[], int n);
+double correlation(const double x[], const double y[], int n);
+double geometricMean(const double data[], int n);
+double harmonicMean(const double data[], int n);
+double rootMeanSquare(const double data[], int n);
+
+// Essential helpers
+void copyArray(const double src[], double dest[], int n);
+void printSummaryStats(const double data[], int n);
+
 #endif
